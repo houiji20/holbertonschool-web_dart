51dc333e2c505eeaad9bc70340430fdb4a63e0fe
houiji20 <91070531+houiji20@users.noreply.github.com>
2024-01-28 22:57:51 +0100

    Update 1-get_user_id.dart

--- 0x03-asynchronous/1-get_user_id.dart
+++ 0x03-asynchronous/1-get_user_id.dart
@@ -1,7 +1,11 @@
 import '1-util.dart';
 
 Future<String> getUserId() async {
-  String userData = await fetchUserData();
-  Map<String, dynamic> userMap = jsonDecode(userData);
-  return userMap['id'];
+  try {
+    String userData = await fetchUserData();
+    Map<String, dynamic> userMap = jsonDecode(userData);
+    return userMap['id'];
+  } catch (error) {
+    return 'error caught: $error';
+  }
 }
